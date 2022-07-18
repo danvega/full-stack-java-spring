798282d705d4858a3e4fd805a6655f5f94096e19
Dan Vega <danvega@gmail.com>
2022-07-18 16:52:01 -0400

    removing commented out @CrossOrigin annotation

--- reading-list-mono/src/main/java/dev/danvega/readinglist/controller/BookController.java
+++ reading-list-mono/src/main/java/dev/danvega/readinglist/controller/BookController.java
@@ -2,7 +2,6 @@ package dev.danvega.readinglist.controller;
 
 import dev.danvega.readinglist.model.Book;
 import dev.danvega.readinglist.repository.BookRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
