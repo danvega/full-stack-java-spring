f82440c67fe76bfab99a40d1fbc9b321f904034a
Dan Vega <danvega@gmail.com>
2022-07-18 11:35:37 -0400

    Reading List bootstrapped from start.spring.io

---
+++ reading-list/src/test/java/dev/danvega/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
